c2c30d512f0ed82ca6a879483d2bdcf3998835da
Apar Agarwal <aparagarwal01@gmail.com>
2026-03-15 04:23:43 +0530

    Refactor app.js to set up routing and error handling middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 import express from "express";
+import userRouter from "./routes/user.routes.js";
+import staticRouter from "./routes/static.routes.js";
+import { errorHandler } from "./middlewares/error.middleware.js";
+import { ApiError } from "./utils/ApiError.js";
 
 const app = express();
 
-app.get('/', (req, res)=>{
-    res.send("API is up and running!");
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', staticRouter);
+app.use('/user', userRouter);
+
+// 404 - handler
+app.use((req, res, next) => {
+    next(new ApiError(404, "Page Not Found"));
+});
+// Global Error handler
+app.use(errorHandler);
 
 export default app;
