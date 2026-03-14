ebeea4625794f998723257d3fc2603baa7e647aa
Apar Agarwal <aparagarwal01@gmail.com>
2026-03-15 00:36:32 +0530

    Implement database connection and update server initialization

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
 import { configDotenv } from "dotenv";
 configDotenv();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
-    console.log("Server listening on http://localhost:3000");
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server listening on http://localhost:3000");
+    })
+}).catch((error) => {
+    console.error("ERROR: Failed to connect to the database. Make sure the database is up and running!");
 })
