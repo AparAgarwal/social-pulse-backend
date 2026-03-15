15d5d8bfa8d88e00907c0612d748caebd11cce5e
Apar Agarwal <aparagarwal01@gmail.com>
2026-03-15 12:18:32 +0530

    Add cookie-parser middleware for handling cookies in requests

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
@@ -19,6 +20,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/user', userRoutes);
