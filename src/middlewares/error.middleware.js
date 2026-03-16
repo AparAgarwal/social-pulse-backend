21145896f127573ff24709da444768a8784558a8
Apar Agarwal <aparagarwal01@gmail.com>
2026-03-16 07:25:37 +0530

    Add error handling for Multer file size limit in error middleware

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -3,6 +3,14 @@ import { ApiError } from "../utils/ApiError.js";
 const errorHandler = (err, req, res, next) => {
     let error = err;
 
+    if (error?.name === 'MulterError') {
+        if (error.code === 'LIMIT_FILE_SIZE') {
+            error = new ApiError(400, 'Image size exceeds 2MB limit');
+        } else {
+            error = new ApiError(400, error.message);
+        }
+    }
+
     // If the error isn't already an ApiError, wrap it so it's consistent
     if (!(error instanceof ApiError)) {
         const statusCode = error.statusCode || 500;
